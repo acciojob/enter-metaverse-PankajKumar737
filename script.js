e3c8074ae66b81f59503c7015fdaa439b998de07
Pankaj Kumar <pankajkumar123014@gmail.com>
2023-04-26 11:02:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let p = document.getElementById("status");
+let btn = document.getElementById("enterBtn");
+
+btn.addEventListener("click", ()=>{
+	p.innerText="Entered Metaverse"
+});
+
